85130990ec4c40b783c2eb2c11df684ca7be6b2f
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2020-03-24 12:37:21 +0100

    adding the names for the specific columns of each dataset

---
+++ utilities/dataset_constants.py
@@ -0,0 +1,6 @@
+job_id_cols = {'onet': 'O*NET-SOC Code',
+               'esco': 'conceptUri'}
+skill_relations_id_cols = {'esco': 'occupationUri', 'onet': 'O*NET-SOC Code'}
+main_titles = {'onet': 'Title', 'esco': 'preferredLabel'}
+alternative_titles = {'onet': None, 'esco': 'altLabels'}
+skill_labels = {'onet': 'Task', 'esco': 'description_skill'}
--- utilities/esco_utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-ESCO_LABEL = 'preferredLabel'
-ESCO_ALT_LABELS = 'altLabels'
-ESCO_DESC = 'description'
-ESCO_LEVEL = 'reuseLevel'
-
