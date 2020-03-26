aaa4cc720f15d08e9b776d919d6fb234c54f9f17
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2020-03-26 15:12:59 +0100

    added significance testing to find_top_words

--- utilities/pandas_utils.py
+++ utilities/pandas_utils.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from functools import reduce
+from scipy.sparse import csr_matrix
+import numpy as np
+from utilities.common_utils import *
 
 def make_two_cols_map(df, col_key, col_val):
     """
@@ -29,3 +33,31 @@ def make_indexed_columns(df, col_names, reverse=None):
         else:
             result_list.append(make_two_cols_map(df, colname, 'index'))
     return result_list
+
+def find_top_words(df, colname, tfidf_model, top_n=10, lower_bound=None, modify_df=True):
+    vocab = invert_dict(tfidf_model.vocabulary_)
+    if isinstance(df.iloc[0][colname], csr_matrix):
+        df[colname] = df[colname].apply(lambda x: np.array(x.todense()).flatten())
+    if lower_bound is None:
+        new_col = df[colname].apply(lambda x: ([vocab[y] for y in
+                            np.argsort(x)[-top_n:]])[::-1])
+    else:
+        new_col = df[colname].apply(lambda x: ([vocab[y] for y in
+                            np.argsort(x)[-top_n:] if x[y] > lower_bound])[::-1])
+    if modify_df:
+        df['top_words_'+colname.split('_')[1]] = new_col
+        return df
+    else:
+        return new_col
+
+def aggregate_top_words(df, colname, col_to_aggregate_on, tfidf_model, top_n=10, raw_tf_idf=False):
+    if not raw_tf_idf:
+        return find_top_words(df[[col_to_aggregate_on, colname]].\
+                groupby(col_to_aggregate_on).sum(),
+                colname, tfidf_model, top_n=top_n)[['top_words_'+colname.split('_')[1]]]
+    else:
+        return df[[col_to_aggregate_on, colname]].\
+                groupby(col_to_aggregate_on).sum()
+
+def reduce_sum(series):
+    return reduce(lambda x, y: x + y, series)
