27444c1bd853c3879c76e15909620a1b4ca420da
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2020-04-09 08:30:55 +0200

    debug

--- create_initial_dfs.py
+++ create_initial_dfs.py
@@ -36,8 +36,7 @@ def main():
                                            join_series_with_char}).reset_index()
     onet_jobs = pd.merge(onet_jobs, alternate_labels, on='O*NET-SOC Code')
     onet_jobs = pd.merge(onet_jobs, onet_career_clusters[['Code', 'Career Cluster', 'Career Pathway']],
-                         left_on='O*NET-SOC Code', right_on='Code')
-    print(onet_jobs.columns)
+                         left_on='O*NET-SOC Code', right_on='Code').drop(columns=['Code'])
 
     if w2v_model is not None:
         esco_jobs_word_vectors = get_df_word_vectors(esco_jobs, ['preferredLabel', 'altLabels'], w2v_model)
@@ -50,7 +49,7 @@ def main():
     esco_skills = pd.read_csv(os.path.join(args.esco_dir,'skills_en.csv'))
     esco_job_skill_relations = pd.read_csv(
         os.path.join(args.esco_dir,'occupationSkillRelations.csv'))
-    onet_skills_and_relations = pd.read_csv(os.path.join(args.onet_dir,'/Task Statements.txt'),
+    onet_skills_and_relations = pd.read_csv(os.path.join(args.onet_dir,'Task Statements.txt'),
                                             sep='\t')
 
     # Preparing O*NET skills and relations data
@@ -59,8 +58,7 @@ def main():
     onet_skills_and_relations['is_essential'] = onet_skills_and_relations['Task Type'].apply(lambda x: x == 'Core')
     onet_skills_and_relations = pd.merge(onet_skills_and_relations,
                                          onet_career_clusters[['Code', 'Career Cluster', 'Career Pathway']],
-                                                left_on='O*NET-SOC Code', right_on='Code')
-    print(onet_skills_and_relations.columns)
+                                                left_on='O*NET-SOC Code', right_on='Code').drop(columns=['Code'])
 
     # Preparing ESCO skills and relations data
     esco_skills_and_relations = pd.merge(pd.merge(esco_skills, esco_job_skill_relations.drop(columns=['skillType']),
