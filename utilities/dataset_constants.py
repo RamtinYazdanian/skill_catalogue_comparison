a7563f4362f03f44f0a1d38a5b93e1bf48e8e21b
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2020-04-16 13:18:04 +0200

    alternate titles updated

--- utilities/dataset_constants.py
+++ utilities/dataset_constants.py
@@ -3,5 +3,5 @@ JOB_ID_COLS = {'ONET': 'O*NET-SOC Code',
                'SWISS': 'job_en'}
 SKILL_RELATIONS_ID_COLS = {'ESCO': 'occupationUri', 'ONET': 'O*NET-SOC Code', 'SWISS': 'job_en'}
 MAIN_TITLES = {'ONET': 'Title', 'ESCO': 'preferredLabel', 'SWISS': 'job_en'}
-ALT_TITLES = {'ONET': None, 'ESCO': 'altLabels', 'SWISS': None}
+ALT_TITLES = {'ONET': 'Alternate Title', 'ESCO': 'altLabels', 'SWISS': None}
 SKILL_LABELS = {'ONET': 'Task', 'ESCO': 'description_skill', 'SWISS': 'task_en'}
